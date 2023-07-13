82c63e9700c6100918bb1f9cda23cb7af7f9c727
MerwanDev <mlaakad@yahoo.com>
2023-07-13 14:23:23 +0200

    fix nomatch

--- src/router/NoMatch.js
+++ src/router/NoMatch.js
@@ -1,17 +1,12 @@
-import { Link, useLocation } from 'react-router-dom';
-
 import React from 'react';
 import NotFound404 from '../pages/404';
 
+const NoMatch = () => {
   return (
     <center>
-      <h2>404</h2>
-      <code>Page : {location.pathname}</code>
-      <br />
-
-      <Link to="/" className="btn btn-primary">
-        Retour sur la page princpale !
-      </Link>
+      <NotFound404 />
     </center>
   );
-}
+};
+
+export default NoMatch;
